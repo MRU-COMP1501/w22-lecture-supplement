8659763d95137621483c8ff76171ab8dff5b77ce
Charlotte Curtis <ccurtis@mtroyal.ca>
2022-01-18 15:01:10 -0700

    Adding code written during lecture 03

---
+++ lecture03.py
@@ -0,0 +1,21 @@
+miles = 26.2
+kilometers = miles * 1.6
+print(kilometers)
+print(miles)
+
+# spaces in variable name = 5 # error, no spaces allowed
+camelCaseVariable = 5 # allowed, but not Python style
+snake_case_variable = 5 # Python style
+
+# print = 7 # Don't do this, it redefines the print function to 7
+# print(print)
+
+x = 5
+x = x + 1
+print(x)
+
+# x + 1 = x # SyntaxError
+x += 1
+print(x)
+
+y = x * 5
